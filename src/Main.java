f5e944d172d6f5764bef720655c4c2239cdc1ff2
KkaahhLLiihh <157661571+KkaahhLLiihh@users.noreply.github.com>
2024-03-14 08:00:01 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("MP3", "Máy nghe nhạc", "1234", 100.0f, 200);
+        Book book = new Book("Lập trình Java", "Lập trình Java căn bản", "2345", 50.0f, "Phạm Văn Trung", 650, "Programming");
 
-        MP3 mp3;
+        System.out.println("MP3 Information:");
         mp3.showInfo();
 
-        Book book;
+        System.out.println("\nBook Information:");
         book.showInfo();
-
     }
 }
